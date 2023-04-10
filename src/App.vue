faa0575541a988501ef00bb740e1bbb954ac0914
Maciej-91 <maciek.2205.mk@gmail.com>
2023-04-10 13:08:44 +0200

    add connection page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Connection</router-link> |
+      <router-link to="/about">Créer un compte</router-link>
     </div>
     <router-view/>
   </div>
